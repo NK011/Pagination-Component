1c3d06c2d27549f025f87d00681d44407c608f58
NK011 <nitish.kumar@geeksforgeeks.org>
2024-09-29 14:48:48 +0530

    Initial Commit - Build the pagination component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    const [activePage, setActivePage] = useState(1);
+    const [startIndex, setStartIndex] = useState(0);
+    const [paginationBtns, setPaginationBtns] = useState(1);
+
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch(
+                "https://dummyjson.com/products?limit=100"
+            )
+                .then((res) => res.json())
+                .then((data) => data);
+
+            setProducts(response?.products);
+            setPaginationBtns(Math.ceil(response?.products.length / 10));
+        } catch (e) {
+            console.log(e);
+            // window.alert("Error fetching products");
+        }
+    };
+
+    const handlePageChange = (page) => {
+        const newIndex = (page - 1) * 10;
+        setActivePage(page);
+        setStartIndex(newIndex);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="products__list">
+                {products.slice(startIndex, startIndex + 10).map((prd) => (
+                    <div className="prodcut-card" key={prd.id}>
+                        <img src={prd.thumbnail} alt="prodcut__image" />
+                        <div className="product__info">
+                            <h4>{prd.title}</h4>
+                            <p>Price: ${prd.price}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="products__pagination">
+                {[...Array(paginationBtns).keys()].map((p, i) => (
+                    <button
+                        onClick={() => handlePageChange(i + 1)}
+                        className={`pagination__btn ${
+                            activePage === i + 1 ? "active" : ""
+                        }`}
+                        key={i + 1}
+                    >
+                        {i + 1}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
